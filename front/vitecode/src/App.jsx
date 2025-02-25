d1960e647768825215775b8dfd3079479fe1620f
mmessiasdev <mmessiasdev@gmail.com>
2025-02-25 15:16:27 -0300

    first commit

---
+++ front/vitecode/src/App.jsx
@@ -0,0 +1,16 @@
+import MapComponent from './components/maps/component'
+
+function App() {
+
+  return (
+    <>
+      <div style={{ height: "100vh", width: "100%", color: "#f4f4f4", display: "flex"}}>
+        <MapComponent />
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
